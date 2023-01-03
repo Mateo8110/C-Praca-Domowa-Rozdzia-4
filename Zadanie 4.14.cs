ca1488ff3b95e031ffe5f0b13c2243712b127092
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:42:24 +0100

    Add files via upload
    Zadanie 4.14.
    W danej firmie środki trwałe mają identyfikatory złożone z kilku liter, myślnika oraz
    czterech cyfr. Te cztery cyfry to rok zakupu danego środka trwałego. Przykładowe
    identyfikatory to: KOMG-2002, BH-2010. Napisz program, który deklaruje 5-cio elementową
    tablicę typu string dla środków trwałych, którą należy zainicjalizować przykładowymi
    identyfikatorami w czasie deklaracji. Program ma dla każdego środka trwałego podać liczbę
    lat, jakie upłynęły od jego zakupu.

---
+++ Zadanie 4.14.cs
@@ -0,0 +1,31 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            string[] tab = { "komp-2010", "lap-2012", "ps-2014", "xbox-2018", "lodowka-2021" };
+            string[] sub;
+            var str = String.Join("-", tab);
+            Console.WriteLine(str);
+
+            sub = str.Split('-');
+
+            for (int i = 0; i < sub.Length; i++)
+            {
+                Console.Write("Dla produktu ");
+                Console.Write(sub[i]);
+                Console.Write(" data zakupu to: ");
+                i += 1;
+                Console.Write(sub[i]);
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
