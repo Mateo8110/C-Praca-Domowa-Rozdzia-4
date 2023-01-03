0772c9f2c4b00aba9bb4356494d13e49327e0541
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:40:34 +0100

    Add files via upload

---
+++ Zadanie 4.11.cs
@@ -0,0 +1,61 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst: ");
+            string tekst = (Console.ReadLine());
+
+            int a = 0;
+            foreach (char litera in tekst)
+                if (litera == 'a')
+                    a++;
+
+            int b = 0;
+            foreach (char litera in tekst)
+                if (litera == 'b')
+                    b++;
+
+            int c = 0;
+            foreach (char litera in tekst)
+                if (litera == 'c')
+                    c++;
+
+            int d = 0;
+            foreach (char litera in tekst)
+                if (litera == 'd')
+                    d++;
+            int e = 0;
+            foreach (char litera in tekst)
+                if (litera == 'e')
+                    e++;
+
+            int f = 0;
+            foreach (char litera in tekst)
+                if (litera == 'f')
+                    f++;
+
+
+
+            Console.WriteLine("Litera a wystąpiła {0} razy", a);
+            Console.WriteLine("Litera b wystąpiła {0} razy", b);
+            Console.WriteLine("Litera c wystąpiła {0} razy", c);
+            Console.WriteLine("Litera d wystąpiła {0} razy", d);
+            Console.WriteLine("Litera e wystąpiła {0} razy", e);
+            Console.WriteLine("Litera f wystąpiła {0} razy", f);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
