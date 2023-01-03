3c03b1ed8484ef1ad62a597d11a0c796d104a5be
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:40:58 +0100

    Add files via upload

---
+++ Zadanie 4.12.cs
@@ -0,0 +1,35 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "W parę godzin później, gdy noc zbierała się do odejścia,\n" +
+            "Puchatek obudził się nagle z uczuciem dziwnego przygnębienia.\n" +
+            "To uczucie dziwnego przygnębienia miewał już nieraz i wiedział,\n" +
+            "co ono oznacza. Był głodny. Więc poszedł do spiżarni,\n" +
+            "wgramolił się na krzesełko, sięgnął na górną półkę, ale nic nie znalazł.";
+
+            Console.WriteLine(tekst);
+
+            int licznik = 1;
+            int start = 0;
+
+            while ((start = tekst.IndexOf('\n', start + 1)) >= 0)
+            {
+                licznik++;
+            }
+            Console.WriteLine("\n Liczba wierszy w tekscie: {0}", licznik);
+            Console.WriteLine();
+
+            string[] podzial = tekst.Split('\n');
+            for (int i = 0; i < podzial.Length; i++)
+            {
+                Console.WriteLine("W wierszu '{0}', mamy {1} znaków.", podzial[i], podzial[i].Length);
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n Ilosc znaków w całym tekscie: {0}", tekst.Length);
+            Console.ReadKey();
+        }
+    }
+}
