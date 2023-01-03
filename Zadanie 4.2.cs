28fa2fec1cbe523ba1fd5934e6a4b1575cc0c7cd
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:36:05 +0100

    Add files via upload

---
+++ Zadanie 4.2.cs
@@ -0,0 +1,32 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tab1 = new int[10];
+            int[] tab2 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Podaj {0} liczbę", i + 1);
+                tab1[i] = Convert.ToInt32(Console.ReadLine());
+
+                 
+               for (int j = tab1.Length - 1; j >= 0; j--)
+               {
+                   if (tab1[j] > 0)
+                       tab2[j] += tab1[j];
+               }
+           
+            }
+            Console.WriteLine("Dodatnie liczby to: ");
+            for (int i = 0; i < tab2.Length; i++)
+            {
+                if (tab2[i] > 0)
+                    Console.Write(tab2[i] + ", ");
+            }
+         Console.ReadKey();
+        }
+    }
+}
