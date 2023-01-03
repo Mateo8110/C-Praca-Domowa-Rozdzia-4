ad970ee01320529e4bffd01d2b4757d6f88bb5ba
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:39:45 +0100

    Add files via upload

---
+++ Zadanie 4.9.cs
@@ -0,0 +1,18 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+            Console.WriteLine("Wprowadz tekst: ");
+            tekst = Console.ReadLine();
+            int liczbaWyrazów = 1;
+            foreach (char litera in tekst)
+                if (litera == ' ')
+                    liczbaWyrazów++;
+            Console.WriteLine("Wprowadzony tekst zawiera {0} wyrazow", liczbaWyrazów);
+            Console.ReadKey();
+        }
+    }
+}
