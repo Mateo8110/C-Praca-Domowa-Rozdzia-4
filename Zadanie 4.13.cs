d63c836de70022d1c7a81ce0435dac174327b7a6
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:41:24 +0100

    Add files via upload

---
+++ Zadanie 4.13.cs
@@ -0,0 +1,42 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            string text = "Kiedy idzie się po miód z balonikiem, to trzeba się starać, żeby pszczoły nie wiedziały, po co się idzie – odpowiedział Puchatek";
+            string[] tablicaSlow = text.Split();
+            int licznik = 1;
+
+            for (int x = 0; x < tablicaSlow.Length; x++)
+            {
+                for (int y = 0; y < tablicaSlow.Length; y++)
+                {
+                    if (tablicaSlow[x].Equals(tablicaSlow[y]) == true && y != x)
+                    {
+                        tablicaSlow[y] = "null";
+                        if (tablicaSlow[x] == "null")
+                        {
+                            licznik -= 2;
+                        }
+                        licznik++;
+                    }
+                }
+                if (licznik > 0)
+                {
+                    tablicaSlow[x] = tablicaSlow[x] + " - " + licznik + " raz-y ";
+                }
+                licznik = 1;
+            }
+            foreach (var x in tablicaSlow.Distinct())
+            {
+                if (x != "null")
+                {
+                    Console.Write(x);
+                }
+                licznik++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
