969697e09c4acbfedaff74e634d1b0653b72aad4
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:35:39 +0100

    Add files via upload

---
+++ Zadanie 4.1.cs
@@ -0,0 +1,23 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę elementów w tablicy: ");
+            int rozmiar = Convert.ToInt32(Console.ReadLine());
+            int[] liczby = new int[rozmiar];
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                Console.WriteLine("Podaj {0} liczbę", i + 1);
+                liczby[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                Console.Write(liczby[i] + ", ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
